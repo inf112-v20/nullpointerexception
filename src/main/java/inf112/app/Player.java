b41c1840a5d49b332d216a5cdc166c924bef6d4c
Lasse Vinsjevik <lvi033@uib.no>
2020-02-14 15:44:49 +0100

    Player Texture now faces the player Direction

--- src/main/java/inf112/app/Player.java
+++ src/main/java/inf112/app/Player.java
@@ -1,33 +1,41 @@
-package inf112.skeleton.app;
+package inf112.app;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Map;
+
 public class Player extends InputAdapter {
 
-    private TiledMapTileLayer playerLayer;
     private TiledMapTileLayer.Cell playerCell;
     private TiledMapTileLayer.Cell playerDiedCell;
     private TiledMapTileLayer.Cell playerWonCell;
     //Vector holds players position
     private Vector2 player;
+    private Directions direction;
+    private Game game;
+
+    private Map<String, TiledMapTileLayer> layers;
 
     /**
      * Initializing default/dying/winning cells of a player.
      * Texture region splits the player texture into 3 different textures and puts them in a 2-dimensional array.
      * Initializing the inputProcessor for input-listening.
      *
-     * @param board board object
+     * @param layers board object
+     * @param game   game object
      */
-    public Player(TiledMap board) {
-        playerLayer = (TiledMapTileLayer) board.getLayers().get("Player");
+    public Player(Map<String, TiledMapTileLayer> layers, Game game) {
+        this.layers = layers;
+        this.game = game;
+        direction = Directions.SOUTH;
+
         Texture playerTexture = new Texture("assets/player.png");
         TextureRegion[][] playerPictures = TextureRegion.split(playerTexture,
                 (int) Board.TILE_SIZE,
@@ -47,15 +55,6 @@ public class Player extends InputAdapter {
     }
 
     /**
-     * Returns a layer consisting of players
-     *
-     * @return the layer with player(s)
-     */
-    public TiledMapTileLayer getLayer() {
-        return playerLayer;
-    }
-
-    /**
      * Refreshing the former players position to null
      * Implements the board-movement of a player
      * Prints out the current position
@@ -65,16 +64,32 @@ public class Player extends InputAdapter {
      */
     @Override
     public boolean keyUp(int keycode) {
-        playerLayer.setCell((int) player.x, (int) player.y, null);
+        layers.get("player").setCell((int) player.x, (int) player.y, null);
 
         if (keycode == Input.Keys.RIGHT)
-            player.x += 1;
+            if (game.canMove(getX() + 1, getY(), direction) && direction == Directions.EAST)
+                player.x += 1;
+            else
+                direction = Directions.EAST;
+
         if (keycode == Input.Keys.LEFT)
-            player.x -= 1;
+            if (game.canMove(getX() - 1, getY(), direction) && direction == Directions.WEST)
+                player.x -= 1;
+            else
+                direction = Directions.WEST;
+
         if (keycode == Input.Keys.UP)
-            player.y += 1;
-        if (keycode == Input.Keys.DOWN)
-            player.y -= 1;
+            if (game.canMove(getX(), getY() + 1, direction) && direction == Directions.NORTH)
+                player.y += 1;
+            else
+                direction = Directions.NORTH;
+
+        if (keycode == Input.Keys.DOWN) {
+            if (game.canMove(getX(), getY() - 1, direction) && direction == Directions.SOUTH)
+                player.y -= 1;
+            else
+                direction = Directions.SOUTH;
+        }
         return super.keyDown(keycode);
     }
 
@@ -110,8 +125,8 @@ public class Player extends InputAdapter {
      *
      * @return x value
      */
-    public float getX() {
-        return player.x;
+    public int getX() {
+        return (int) player.x;
     }
 
     /**
@@ -119,7 +134,49 @@ public class Player extends InputAdapter {
      *
      * @return y value
      */
-    public float getY() {
-        return player.y;
+    public int getY() {
+        return (int) player.y;
+    }
+
+    /**
+     * Checks which imagine to show to the screen depending on player state
+     */
+    public void checkPlayerState() {
+        //If a player is on the position of the flag, change the image of a player to winning-state
+        if (layers.get("flag").getCell(getX(), getY()) != null)
+            layers.get("player").setCell(getX(), getY(), setPlayerToWon());
+            //If a player stands on the hole-cell, change the image to dying-state
+        else if (layers.get("hole").getCell(getX(), getY()) != null)
+            layers.get("player").setCell(getX(), getY(), setPlayerToDead());
+        else
+            layers.get("player").setCell(getX(), getY(), setPlayerToDefault());
+
+        checkPlayerDirection();
+    }
+
+    /**
+     * Checks which direction the player is facing, and rotates the texture accordingly
+     */
+    private void checkPlayerDirection() {
+        if (direction == Directions.NORTH) {
+            playerCell.setRotation(2);
+            playerDiedCell.setRotation(2);
+            playerWonCell.setRotation(2);
+        }
+        if (direction == Directions.EAST) {
+            playerCell.setRotation(1);
+            playerDiedCell.setRotation(1);
+            playerWonCell.setRotation(1);
+        }
+        if (direction == Directions.SOUTH) {
+            playerCell.setRotation(0);
+            playerDiedCell.setRotation(0);
+            playerWonCell.setRotation(0);
+        }
+        if (direction == Directions.WEST) {
+            playerCell.setRotation(3);
+            playerDiedCell.setRotation(3);
+            playerWonCell.setRotation(3);
+        }
     }
 }
--- src/test/java/inf112/app/BoardTest.java
+++ src/test/java/inf112/app/BoardTest.java
@@ -1,4 +1,4 @@
-package inf112.skeleton.app;
+package inf112.app;
 
 import org.junit.Before;
 import org.junit.Test;
