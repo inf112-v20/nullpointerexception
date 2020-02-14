59f7a80b9857e67a9e67c3fc8b11ef633f6fc956
Lasse Vinsjevik <lvi033@uib.no>
2020-02-14 15:33:36 +0100

    Refactored methods and removed empty methods Added wall detection

---
+++ src/main/java/inf112/app/Game.java
@@ -0,0 +1,87 @@
+package inf112.app;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class Game extends ApplicationAdapter {
+    private Board board;
+    private Player player;
+
+    private OrthogonalTiledMapRenderer renderer;
+
+    /**
+     * Initializing a board, camera, renderer and player in addition to creating the needed TiledMap layers.
+     */
+    @Override
+    public void create() {
+        board = new Board();
+        player = new Player(board.getBoardLayers(), this);
+
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false,
+                board.getBoardWidth() * Board.TILE_SIZE,
+                board.getBoardHeight() * Board.TILE_SIZE);
+        camera.update();
+
+        renderer = new OrthogonalTiledMapRenderer(board.getBoard());
+        renderer.setView(camera);
+    }
+
+    /**
+     * Called when the Application is destroyed. Preceded by a call to pause().
+     */
+    @Override
+    public void dispose() {
+        renderer.dispose();
+    }
+
+    /**
+     * A loop method which renders the changes on the screen
+     * Shows the player default/winning/dying state on the board
+     */
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+
+        player.checkPlayerState();
+
+        renderer.render();
+    }
+
+    /**
+     * Checks if there is a wall in the current tile or the next tile
+     * which the player is trying to move into.
+     *
+     * @param newX x coordinate player is trying to move towards
+     * @param newY y coordinate player is trying to move towards
+     * @param dir  direction the player is facing/wanting to move towards
+     * @return boolean true or false if the tile has a wall
+     */
+    private boolean hasWall(int newX, int newY, Directions dir) {
+        //Checks if there is a wall in the direction the player is trying to move
+        if (board.getBoardLayers().get("wall" + Directions.getName(dir)).getCell(player.getX(), player.getY()) != null)
+            return true;
+        //Checks if there is a wall in the next tile, from the direction the player is coming from
+        return board.getBoardLayers().get("wall" + Directions.getName(Directions.reverse(dir))).getCell(newX, newY) != null;
+    }
+
+    /**
+     * Checks if the player can move in a certain direction
+     *
+     * @param newX x coordinate player is trying to move towards
+     * @param newY y coordinate player is trying to move towards
+     * @param dir  direction the player is facing/wanting to move towards
+     * @return boolean true or false if player can move
+     */
+    public boolean canMove(int newX, int newY, Directions dir) {
+        if (hasWall(newX, newY, dir))
+            return false;
+        if (newX >= board.getBoardWidth() || newX < 0)
+            return false;
+        return newY < board.getBoardHeight() && newY >= 0;
+    }
+}
