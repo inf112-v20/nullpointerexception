d59bd8145f7203ee3a9e3ae4c478702b0e1da0ef
Lasse Vinsjevik <lvi033@uib.no>
2020-02-14 15:40:29 +0100

    Boards are much more generic and depend more on file input Changed default board size Minor map updates

--- src/main/java/inf112/app/Main.java
+++ src/main/java/inf112/app/Main.java
@@ -1,4 +1,4 @@
-package inf112.skeleton.app;
+package inf112.app;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally";
-        cfg.width = 500;
-        cfg.height = 500;
+        cfg.width = 900;
+        cfg.height = 900;
 
         new LwjglApplication(new Game(), cfg);
     }
