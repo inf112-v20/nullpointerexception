0f8e65e1bcfe7d4f2f10f377e6c0d8caaf59d6e9
Michal Juzyszyn <michal.juzyszyn@student.uib.no>
2020-02-19 18:32:01 +0100

    Fixed an issue with the button not being able to scale with resolution

--- src/main/java/inf112/app/MenuScreen.java
+++ src/main/java/inf112/app/MenuScreen.java
@@ -2,8 +2,14 @@ package inf112.app;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MenuScreen extends ScreenAdapter {
 
@@ -11,14 +17,19 @@ public class MenuScreen extends ScreenAdapter {
     private Texture startButton;
     private Texture startButtonActive;
 
-    public MenuScreen(GameRunner gameRunner){
+
+
+    public MenuScreen(GameRunner gameRunner) {
         this.gameRunner = gameRunner;
         startButton = new Texture("start_game.png");
         startButtonActive = new Texture("start_game_active.png");
     }
 
+
+
     /**
      * Rendering the start button and starting up the game if the button is clicked
+     *
      * @param v
      */
     @Override
@@ -27,24 +38,29 @@ public class MenuScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
         gameRunner.batch.begin();
 
-        //X pos where the image is being drawn
-        int x = GameRunner.SCREEN_WIDTH/2 - startButton.getWidth()/2;
-        //Current mouse position on Y-axes
-        int mousePosition_Y = GameRunner.SCREEN_HEIGHT - Gdx.input.getY();
-        //Current mouse position on X-axes
+        //X and Y pos where the image is being drawn
+        int x = (GameRunner.SCREEN_WIDTH / 2) - (startButton.getWidth() / 2);
+        int y = GameRunner.SCREEN_HEIGHT / 2 -startButton.getHeight()/2;
+
+        //Current mouse position on Y-axis
+        int mousePosition_Y = Gdx.input.getY();
+        //Current mouse position on X-axis
         int mousePosition_X = Gdx.input.getX();
 
+        int scaleX = (mousePosition_X * GameRunner.SCREEN_WIDTH) / Gdx.graphics.getWidth();
+        int scaleY = (mousePosition_Y * GameRunner.SCREEN_HEIGHT) / Gdx.graphics.getHeight();
+
         //If mouse hovers over the button and its being clicked, start the game
-        if(mousePosition_X < x + startButton.getWidth() && mousePosition_X > x
-                && mousePosition_Y < GameRunner.SCREEN_HEIGHT/2 + startButton.getHeight() && mousePosition_Y > GameRunner.SCREEN_HEIGHT/2) {
-            gameRunner.batch.draw(startButtonActive, x, GameRunner.SCREEN_WIDTH/2);
+        if (scaleX < x + startButton.getWidth() && scaleX > x
+                && scaleY < y + startButton.getHeight() && scaleY > y) {
+            gameRunner.batch.draw(startButtonActive, x, y);
             //If mouse is clicked - start the game
-            if(Gdx.input.isTouched()){
+            if (Gdx.input.isTouched()) {
                 gameRunner.setScreen(new Game());
+                dispose();
             }
-        }
-        else {
-            gameRunner.batch.draw(startButton, x, GameRunner.SCREEN_WIDTH/2);
+        } else {
+            gameRunner.batch.draw(startButton, x, y);
         }
         gameRunner.batch.end();
     }
@@ -52,5 +68,8 @@ public class MenuScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         gameRunner.batch.dispose();
+        startButton.dispose();
+        startButtonActive.dispose();
     }
+
 }
