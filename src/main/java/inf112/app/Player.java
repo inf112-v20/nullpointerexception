58f7d3515d7c3f28347e0c85f21532f016fdebd6
Lasse Vinsjevik <lvi033@uib.no>
2020-02-20 16:41:42 +0100

    Added a Position class to enhance readability Refactored Player to use Position instead of just changing x and y values. Removed old textures used for testing

--- src/main/java/inf112/app/Player.java
+++ src/main/java/inf112/app/Player.java
@@ -9,18 +9,15 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
-
 import java.util.Map;
 
 public class Player extends InputAdapter {
 
     private TiledMapTileLayer.Cell playerCell;
-    private TiledMapTileLayer.Cell playerDiedCell;
-    private TiledMapTileLayer.Cell playerWonCell;
     //Vector holds players position
     private Vector2 player;
     private Position pos;
-    private Directions direction;
+    private Directions dir;
     private Game game;
 
     private Map<String, TiledMapTileLayer> layers;
@@ -36,12 +33,7 @@ public class Player extends InputAdapter {
     public Player(Map<String, TiledMapTileLayer> layers, Game game) {
         this.layers = layers;
         this.game = game;
-        direction = Directions.SOUTH;
-
-        Texture playerTexture = new Texture("assets/player.png");
-        TextureRegion[][] playerPictures = TextureRegion.split(playerTexture,
-                (int) Board.TILE_SIZE,
-                (int) Board.TILE_SIZE);
+        dir = Directions.SOUTH;
 
         Texture robotTextures = new Texture("assets/robot.png");
         TextureRegion[][] robotTexture = TextureRegion.split(robotTextures,
@@ -49,8 +41,6 @@ public class Player extends InputAdapter {
                 (int) Board.TILE_SIZE);
 
         playerCell = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(robotTexture[0][0]));
-        playerDiedCell = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerPictures[0][1]));
-        playerWonCell = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerPictures[0][2]));
 
         player = new Vector2();
         pos = new Position((int) player.x, (int) player.y);
@@ -68,65 +58,81 @@ public class Player extends InputAdapter {
      */
     @Override
     public boolean keyUp(int keycode) {
-        layers.get("player").setCell((int) player.x, (int) player.y, null);
-
+        layers.get("player").setCell(pos.getX(), pos.getY(), null);
+/*
         switch(keycode) {
             case Input.Keys.RIGHT:
                 if (direction != Directions.EAST)
                     direction = Directions.EAST;
-                else if (game.outOfMap(getX() + 1, getY()) && game.canMove(getX() + 1, getY(), direction))
-                    player.setZero();
-                else if (game.canMove(getX() + 1, getY(), direction) && direction == Directions.EAST)
-                    player.x += 1;
+                else if (game.outOfBoard(pos.getNextPos(direction)) && game.canMove(pos.getNextPos(direction), direction))
+                    checkpoint();
+                else if (game.canMove(pos.getNextPos(direction), direction))
+                    pos = new Position(pos.getNextPos(direction));
                 break;
             case Input.Keys.LEFT:
                 if (direction != Directions.WEST)
                     direction = Directions.WEST;
-                else if (game.outOfMap(getX() - 1, getY()) && game.canMove(getX() - 1, getY(), direction))
-                    player.setZero();
-                else if (game.canMove(getX() - 1, getY(), direction) && direction == Directions.WEST)
-                    player.x -= 1;
+                else if (game.outOfBoard(pos.getNextPos(direction)) && game.canMove(pos.getNextPos(direction), direction))
+                    checkpoint();
+                else if (game.canMove(pos.getNextPos(direction), direction))
+                    pos = new Position(pos.getNextPos(direction));
                 break;
+            case Input.Keys.UP:
+                if (direction != Directions.NORTH)
+                    direction = Directions.NORTH;
+                else if (game.outOfBoard(pos.getNextPos(direction)) && game.canMove(pos.getNextPos(direction), direction))
+                    checkpoint();
+                else if (game.canMove(pos.getNextPos(direction), direction))
+                    pos = new Position(pos.getNextPos(direction));
+                break;
+            case Input.Keys.DOWN:
+                if (direction != Directions.SOUTH)
+                    direction = Directions.SOUTH;
+                else if (game.outOfBoard(pos.getNextPos(direction)) && game.canMove(pos.getNextPos(direction), direction))
+                    checkpoint();
+                else if (game.canMove(pos.getNextPos(direction), direction))
+                    pos = new Position(pos.getNextPos(direction));
+                game.movePlayer();
+                break;*/
 
+        switch (keycode) {
+            case Input.Keys.RIGHT:
+                if (dir != Directions.EAST)
+                    dir = Directions.EAST;
+                else
+                    game.movePlayer(pos);
+                break;
+            case Input.Keys.LEFT:
+                if (dir != Directions.WEST)
+                    dir = Directions.WEST;
+                else
+                    game.movePlayer(pos);
+                break;
+            case Input.Keys.UP:
+                if (dir != Directions.NORTH)
+                    dir = Directions.NORTH;
+                else
+                    game.movePlayer(pos);
+                break;
+            case Input.Keys.DOWN:
+                if (dir != Directions.SOUTH)
+                    dir = Directions.SOUTH;
+                else
+                    game.movePlayer(pos);
+                break;
         }
 
-        if (keycode == Input.Keys.UP)
-            if (direction != Directions.NORTH)
-                direction = Directions.NORTH;
-            else if (game.outOfMap(getX(), getY() + 1) && game.canMove(getX(), getY() + 1, direction))
-                player.setZero();
-            else if (game.canMove(getX(), getY() + 1, direction))
-                player.y += 1;
-
-
-        if (keycode == Input.Keys.DOWN) {
-            if (direction != Directions.SOUTH)
-                direction = Directions.SOUTH;
-            else if (game.outOfMap(getX(), getY() -1) && game.canMove(getX(), getY() - 1, direction))
-                player.setZero();
-            else if (game.canMove(getX(), getY() - 1, direction) && direction == Directions.SOUTH)
-                player.y -= 1;
-
-        }
+        game.checkCurrentTile(pos);
+        updatePlayerState();
         return super.keyDown(keycode);
     }
 
-    /**
-     * Sets the current player state to dead
-     *
-     * @return the new player state
-     */
-    public TiledMapTileLayer.Cell setPlayerToDead() {
-        return playerDiedCell;
+    public void movePlayer(Position newPos) {
+        pos = new Position(newPos);
     }
 
-    /**
-     * Sets the current player state to won
-     *
-     * @return the new player state
-     */
-    public TiledMapTileLayer.Cell setPlayerToWon() {
-        return playerWonCell;
+    public Position checkpoint() {
+        return pos = new Position(0, 0);
     }
 
     /**
@@ -139,62 +145,18 @@ public class Player extends InputAdapter {
     }
 
     /**
-     * Returns the x position on the grid for the player
-     *
-     * @return x value
-     */
-    public int getX() {
-        return (int) player.x;
-    }
-
-    /**
-     * Returns the y position on the grid for the player
-     *
-     * @return y value
-     */
-    public int getY() {
-        return (int) player.y;
-    }
-
-    /**
      * Checks which imagine to show to the screen depending on player state
      */
-    public void checkPlayerState() {
-        //If a player is on the position of the flag, change the image of a player to winning-state
-        if (layers.get("flag").getCell(getX(), getY()) != null)
-            layers.get("player").setCell(getX(), getY(), setPlayerToWon());
-            //If a player stands on the hole-cell, change the image to dying-state
-        else if (layers.get("hole").getCell(getX(), getY()) != null)
-            layers.get("player").setCell(getX(), getY(), setPlayerToDead());
-        else
-            layers.get("player").setCell(getX(), getY(), setPlayerToDefault());
-
-        checkPlayerDirection();
+    public void updatePlayerState() {
+        layers.get("player").setCell(pos.getX(), pos.getY(), setPlayerToDefault());
+        playerCell.setRotation(dir.getID());
     }
 
-    /**
-     * Checks which direction the player is facing, and rotates the texture accordingly
-     */
-    private void checkPlayerDirection() {
-        if (direction == Directions.NORTH) {
-            playerCell.setRotation(2);
-            playerDiedCell.setRotation(2);
-            playerWonCell.setRotation(2);
-        }
-        if (direction == Directions.EAST) {
-            playerCell.setRotation(1);
-            playerDiedCell.setRotation(1);
-            playerWonCell.setRotation(1);
-        }
-        if (direction == Directions.SOUTH) {
-            playerCell.setRotation(0);
-            playerDiedCell.setRotation(0);
-            playerWonCell.setRotation(0);
-        }
-        if (direction == Directions.WEST) {
-            playerCell.setRotation(3);
-            playerDiedCell.setRotation(3);
-            playerWonCell.setRotation(3);
-        }
+    public Position getPlayerPos() {
+        return pos;
+    }
+
+    public Directions getDirection() {
+        return dir;
     }
 }
--- src/main/java/inf112/app/Position.java
+++ src/main/java/inf112/app/Position.java
@@ -9,4 +9,40 @@ public class Position {
         xCoordinate = x;
         yCoordinate = y;
     }
+
+    public Position(Position pos) {
+        xCoordinate = pos.getX();
+        yCoordinate = pos.getY();
+    }
+
+    public int getX() {
+        return xCoordinate;
+    }
+
+    public int getY() {
+        return yCoordinate;
+    }
+
+    public Position getNextPos(Directions dir) {
+        switch (dir) {
+            case NORTH:
+                return new Position(xCoordinate, yCoordinate + 1);
+            case EAST:
+                return new Position(xCoordinate + 1, yCoordinate);
+            case SOUTH:
+                return new Position(xCoordinate, yCoordinate - 1);
+            case WEST:
+                return new Position(xCoordinate - 1, yCoordinate);
+            default:
+                throw new IllegalStateException("Unexpected value: " + dir);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "xCoordinate=" + xCoordinate +
+                ", yCoordinate=" + yCoordinate +
+                '}';
+    }
 }
