f09ec362717abd182598c924837d61ccdde63454
Lasse Vinsjevik <lvi033@uib.no>
2020-02-28 15:53:23 +0100

    Made test class for Position

---
+++ src/test/java/inf112/app/PositionTest.java
@@ -0,0 +1,44 @@
+package inf112.app;
+
+import inf112.app.player.Direction;
+import inf112.app.player.Player;
+import inf112.app.player.Position;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PositionTest {
+
+    Position pos;
+    int x;
+    int y;
+    Direction dirN;
+
+    @Before
+    public void init() {
+        x = 1;
+        y = 2;
+        dirN = Direction.NORTH;
+        pos = new Position(x, y);
+    }
+
+    @Test
+    public void getXTest() {
+        assertEquals(pos.getX(), 1);
+    }
+
+    @Test
+    public void getYTest() {
+        assertEquals(pos.getY(), 2);
+    }
+
+    @Test
+    public void getNextPosTest() {
+        Position northPos = new Position(1, 3);
+        assertEquals(pos.getNextPos(dirN), northPos);
+    }
+
+
+
+}
