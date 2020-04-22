9967f802da42cd2f0d12b421b3f45d01d994cb60
Michal Juzyszyn <michal.juzyszyn@student.uib.no>
2020-04-22 14:17:43 +0200

    fix

--- src/main/java/inf112/app/screens/GameScreen.java
+++ src/main/java/inf112/app/screens/GameScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -40,7 +41,9 @@ public class GameScreen extends ScreenAdapter {
     private Button powerup;
 
     private OrthographicCamera camera;
-    private ArrayList<Button> hand;
+
+    private ArrayList<Button> dealtCards = new ArrayList<>();
+    private ArrayList<Button> hand = new ArrayList<>();
     public GameScreen() {
         game = new inf112.app.Game();
         board = game.getBoard();
@@ -51,27 +54,35 @@ public class GameScreen extends ScreenAdapter {
 
         move1 = new Button("cards/move1_card.png");
         move1.setButtonCoords(powerdown.getButtonX() + powerdown.getButtonTexture().getWidth() + 500, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(move1);
 
         move2= new Button("cards/move2_card.png");
         move2.setButtonCoords(move1.getButtonX() + move1.getButtonTexture().getWidth() + 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(move2);
 
         move3= new Button("cards/move3_card.png");
         move3.setButtonCoords(move2.getButtonX() + move2.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(move3);
 
         right_turn= new Button("cards/right_turn_card.jpg");
         right_turn.setButtonCoords(move3.getButtonX() + move3.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(right_turn);
 
         left_turn= new Button("cards/left_turn_card.jpg");
         left_turn.setButtonCoords(right_turn.getButtonX() + right_turn.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(left_turn);
 
         backup= new Button("cards/backup_card.jpg");
         backup.setButtonCoords(left_turn.getButtonX() + left_turn.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(backup);
 
         turn_180= new Button("cards/180_turn_card.jpg");
         turn_180.setButtonCoords(backup.getButtonX() + backup.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(turn_180);
 
         powerup= new Button("cards/powerup_card.jpg");
         powerup.setButtonCoords(turn_180.getButtonX() + turn_180.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+        dealtCards.add(powerup);
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false,
@@ -105,22 +116,94 @@ public class GameScreen extends ScreenAdapter {
         int healthpoints = game.getPlayersHitPoints();
         GameRunner.batch.begin();
 
+        //Current mouse position on Y-axis
+        int mousePosition_Y = Gdx.input.getY();
+        //Current mouse position on X-axis
+        int mousePosition_X = Gdx.input.getX();
+
+        /**
+         * Vector point of a current mouse position
+         * unprojecting the mouse position so that it matches the worlds coordinates.
+         * (Default mouse position at Y-axis in Gdx.input is the opposite of our worlds Y-axis coords, thats why
+         * we need to unproject it)
+         */
+        Vector3 input = new Vector3(mousePosition_X, mousePosition_Y, 0);
+        camera.unproject(input);
+
+
         GameRunner.batch.draw(powerdown.getButtonTexture(), powerdown.getButtonX(), powerdown.getButtonY(), 350, 350);
         GameRunner.batch.draw(lifes, 20, board.getBoardHeight() * TILE_SIZE + 50, 150, 150);
         GameRunner.batch.draw(health, 0, board.getBoardHeight() * TILE_SIZE + 50 + 200, 220, 220);
-        GameRunner.batch.draw(move1.getButtonTexture(), move1.getButtonX(), move1.getButtonY(), 500, 600);
+
+        for(Button button:dealtCards) {
+            if (button.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(button);
+                    System.out.println("A card has been added");
+                    //dealtCards.remove(button);
+                }
+            } else {
+                GameRunner.batch.draw(button.getButtonTexture(), button.getButtonX(), button.getButtonY(), 500, 600);
+            }
+        }
+        /**GameRunner.batch.draw(move1.getButtonTexture(), move1.getButtonX(), move1.getButtonY(), 500, 600);
         GameRunner.batch.draw(move2.getButtonTexture(), move2.getButtonX(), move2.getButtonY(), 500, 600);
         GameRunner.batch.draw(move3.getButtonTexture(), move3.getButtonX(), move3.getButtonY(), 500, 600);
         GameRunner.batch.draw(right_turn.getButtonTexture(), right_turn.getButtonX(), right_turn.getButtonY(), 500, 600);
         GameRunner.batch.draw(left_turn.getButtonTexture(), left_turn.getButtonX(), left_turn.getButtonY(), 500, 600);
         GameRunner.batch.draw(turn_180.getButtonTexture(), turn_180.getButtonX(), turn_180.getButtonY(), 500, 600);
         GameRunner.batch.draw(backup.getButtonTexture(), backup.getButtonX(), backup.getButtonY(), 500, 600);
-        GameRunner.batch.draw(powerup.getButtonTexture(), powerup.getButtonX(), powerup.getButtonY(), 500, 600);
+        GameRunner.batch.draw(powerup.getButtonTexture(), powerup.getButtonX(), powerup.getButtonY(), 500, 600);**/
 
         font.getData().setScale(7, 7);
         font.setColor(Color.BLACK);
         font.draw(GameRunner.batch, "x " + lifepoints, 170, board.getBoardHeight() * TILE_SIZE + 50 + lifes.getHeight() / 2);
         font.draw(GameRunner.batch, "x " + healthpoints, 210, board.getBoardHeight() * TILE_SIZE + 400 + health.getHeight()/2);
+
+        /**while(hand.size() < 5) {
+            /**for(Button button : dealtCards) {
+                if (button.buttonIsHovered(input)) {
+                    if (Gdx.input.isTouched()) {
+                        hand.add(button);
+                        System.out.println("A card has been added to the hand");
+                        dealtCards.remove(button);
+                    }
+                }
+            if (move1.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(move1);
+                }
+
+            } else if (move2.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(move2);
+                }
+            } else if (move3.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(move3);
+                }
+            } else if (right_turn.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(right_turn);
+                }
+            } else if (left_turn.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(left_turn);
+                }
+            } else if (backup.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(backup);
+                }
+            } else if (turn_180.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(turn_180);
+                }
+            } else if (powerup.buttonIsHovered(input)) {
+                if (Gdx.input.isTouched()) {
+                    hand.add(powerup);
+                }
+            }
+        }**/
         GameRunner.batch.end();
 
     }
