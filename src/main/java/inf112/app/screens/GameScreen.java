c82eb787df0bd90cfce02e9faeca4179dfd9eb41
Michal Juzyszyn <michal.juzyszyn@student.uib.no>
2020-04-22 13:07:57 +0200

    added card sprites to the screen

--- src/main/java/inf112/app/screens/GameScreen.java
+++ src/main/java/inf112/app/screens/GameScreen.java
@@ -14,6 +14,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import inf112.app.board.Board;
 
+import java.util.ArrayList;
+
 public class GameScreen extends ScreenAdapter {
 
     private Stage stage;
@@ -27,8 +29,18 @@ public class GameScreen extends ScreenAdapter {
     private Button powerdown;
     private Texture lifes;
     private Texture health;
-    private OrthographicCamera camera;
 
+    private Button move1;
+    private Button move2;
+    private Button move3;
+    private Button right_turn;
+    private Button left_turn;
+    private Button backup;
+    private Button turn_180;
+    private Button powerup;
+
+    private OrthographicCamera camera;
+    private ArrayList<Button> hand;
     public GameScreen() {
         game = new inf112.app.Game();
         board = game.getBoard();
@@ -36,6 +48,31 @@ public class GameScreen extends ScreenAdapter {
         powerdown.setButtonCoords(0, board.getBoardHeight() * TILE_SIZE + 500);
         lifes = new Texture("life.png");
         health = new Texture("healthpoints.png");
+
+        move1 = new Button("cards/move1_card.png");
+        move1.setButtonCoords(powerdown.getButtonX() + powerdown.getButtonTexture().getWidth() + 500, board.getBoardHeight() * TILE_SIZE + 200);
+
+        move2= new Button("cards/move2_card.png");
+        move2.setButtonCoords(move1.getButtonX() + move1.getButtonTexture().getWidth() + 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        move3= new Button("cards/move3_card.png");
+        move3.setButtonCoords(move2.getButtonX() + move2.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        right_turn= new Button("cards/right_turn_card.jpg");
+        right_turn.setButtonCoords(move3.getButtonX() + move3.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        left_turn= new Button("cards/left_turn_card.jpg");
+        left_turn.setButtonCoords(right_turn.getButtonX() + right_turn.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        backup= new Button("cards/backup_card.jpg");
+        backup.setButtonCoords(left_turn.getButtonX() + left_turn.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        turn_180= new Button("cards/180_turn_card.jpg");
+        turn_180.setButtonCoords(backup.getButtonX() + backup.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
+        powerup= new Button("cards/powerup_card.jpg");
+        powerup.setButtonCoords(turn_180.getButtonX() + turn_180.getButtonTexture().getWidth()+ 340, board.getBoardHeight() * TILE_SIZE + 200);
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false,
                 board.getBoardWidth() * TILE_SIZE,
@@ -67,13 +104,22 @@ public class GameScreen extends ScreenAdapter {
         int lifepoints = game.getPlayersLifeCount();
         int healthpoints = game.getPlayersHitPoints();
         GameRunner.batch.begin();
+
         GameRunner.batch.draw(powerdown.getButtonTexture(), powerdown.getButtonX(), powerdown.getButtonY(), 350, 350);
-        GameRunner.batch.draw(lifes, 0, board.getBoardHeight() * TILE_SIZE + 50, 150, 150);
+        GameRunner.batch.draw(lifes, 20, board.getBoardHeight() * TILE_SIZE + 50, 150, 150);
         GameRunner.batch.draw(health, 0, board.getBoardHeight() * TILE_SIZE + 50 + 200, 220, 220);
+        GameRunner.batch.draw(move1.getButtonTexture(), move1.getButtonX(), move1.getButtonY(), 500, 600);
+        GameRunner.batch.draw(move2.getButtonTexture(), move2.getButtonX(), move2.getButtonY(), 500, 600);
+        GameRunner.batch.draw(move3.getButtonTexture(), move3.getButtonX(), move3.getButtonY(), 500, 600);
+        GameRunner.batch.draw(right_turn.getButtonTexture(), right_turn.getButtonX(), right_turn.getButtonY(), 500, 600);
+        GameRunner.batch.draw(left_turn.getButtonTexture(), left_turn.getButtonX(), left_turn.getButtonY(), 500, 600);
+        GameRunner.batch.draw(turn_180.getButtonTexture(), turn_180.getButtonX(), turn_180.getButtonY(), 500, 600);
+        GameRunner.batch.draw(backup.getButtonTexture(), backup.getButtonX(), backup.getButtonY(), 500, 600);
+        GameRunner.batch.draw(powerup.getButtonTexture(), powerup.getButtonX(), powerup.getButtonY(), 500, 600);
 
         font.getData().setScale(7, 7);
         font.setColor(Color.BLACK);
-        font.draw(GameRunner.batch, "x " + lifepoints, 150, board.getBoardHeight() * TILE_SIZE + 50 + lifes.getHeight() / 2);
+        font.draw(GameRunner.batch, "x " + lifepoints, 170, board.getBoardHeight() * TILE_SIZE + 50 + lifes.getHeight() / 2);
         font.draw(GameRunner.batch, "x " + healthpoints, 210, board.getBoardHeight() * TILE_SIZE + 400 + health.getHeight()/2);
         GameRunner.batch.end();
 
